a50d8fe4384c24c5cac351af1dfa6239bfce118e
EishaMazhar <eisha.mazhar19@gmail.com>
2021-08-30 15:28:17 +0500

    clean directory

--- pwic_mail_sender.py
+++ pwic_mail_sender.py
@@ -9,11 +9,12 @@ import csv
 
 # Change these variables
 SMTP_HOST = "smtp.gmail.com"
-SMTP_USERNAME = "karachi.chapter@pwic.org"
+SMTP_USERNAME = "eisha.mazhar19@gmail.com"
 SMTP_PASSWORD = ""
-SENDER_EMAIL = "karachi.chapter@pwic.org"
+SENDER_EMAIL = "eisha.mazhar19@gmail.com"
 SENDER_NAME = "Eisha Tir Raazia"
-
+CC = "k173730@nu.edu.pk"
+# CC = ["vicepresident@pwic.org", "muazma@pwic.org", "muazma@pwic.org"]
 BODY ="""Hi -<br><br/>
 Thank you for being a huge part of Local Hack Day: Share (2020) by PWiC, Karachi! We are so happy you spent your day building and demoing at Local Hack Day. We wanted to make sure you get recognized for attending as well. You can print out your certificate below to share with your school.<br>
 <br>
@@ -30,12 +31,6 @@ They can also claim the free Azure credits, Adobe XD, or other resources until t
 <a
     href="https://mlh.az1.qualtrics.com/jfe/form/SV_ePRi23xdJG1iCq1">🚩 Free Adobe XD For Students
 </a><br>
-<a
-    href="https://education.github.com/pack">🚩 GitHub Student Developer Pack
-</a><br>
-<br>
-Link to the resources (slides and code that were presented in sessions): <a href="https://drive.google.com/drive/folders/150vTilq7iGNduSVYHPKRispLjwPKBo0z?usp=sharing">Resources Link</a></a><br>
-<br>
 <a href="https://www.facebook.com/pakistaniwomenincomputing/">PWiC's page link, Click here!</a><br>
 <br>
 <a href="https://www.youtube.com/watch?v=Ze0YSQHM16o&t=9172s">Session's YouTube link, Click here!</a><br><br/>
@@ -46,3 +41,33 @@ Happy Hacking,<br>
 Team PWiC, Karachi."""
 
 SUBJECT = "Thank you for attending Local Hack Day: Share; by PWiC, Karachi!"
+
+
+def send_email(email):
+    # RECEIVER_NAME = name
+    RECEIVER_EMAIL = email
+
+    # Create a multipart message and set headers
+    message = MIMEMultipart()
+    message["From"] = str(Header(SENDER_NAME+' <' + SENDER_EMAIL + '>'))
+    message["To"] = str(Header('<' + RECEIVER_EMAIL + '>'))
+    message["SUBJECT"] = SUBJECT
+    message["cc"] = CC
+    # message["Bcc"] = RECEIVER_EMAIL  # Recommended for mass emails
+    print(message)
+    # Add body to email
+    message.attach(MIMEText(BODY, "html"))
+
+    text = message.as_string()
+
+    # Log in to server using secure context and send email
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(SMTP_HOST, 465, context=context) as server:
+        server.login(SMTP_USERNAME, SMTP_PASSWORD)
+        server.sendmail(SENDER_EMAIL, RECEIVER_EMAIL, text)
+
+
+csv_data = csv.reader(open('processed.csv'))
+for i, row in enumerate(csv_data):
+    send_email(row[0])
+    print("Email sent to: " + row[0] + ", please remove this row from CSV before re-running script again (in case of any failure)")
